7bc7ccc78f39e369d801a1d6a7ef5ffe7e37ffc6
mariagres07 <mariagresia347@gmail.com>
2024-04-08 22:53:57 +0700

    Merge origin/master
    Conflicts:
    	src/main/java/org/example/EnhancedLab.java
    
    UTS Commit

---
+++ src/main/java/UTS/Mahasiswa.java
@@ -0,0 +1,71 @@
+package UTS;
+
+public class Mahasiswa {
+
+    String nama;
+    String nim;
+    double nilaiUts;
+    double nilaiUas;
+    double nilaiUh;
+
+    public Mahasiswa(String nama, String nim, double nilaiUh, double nilaiUts, double nilaiUas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.nilaiUh = nilaiUh;
+        this.nilaiUts = nilaiUts;
+        this.nilaiUas = nilaiUas;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public void setNilaiUts(double nilaiUts) {
+        this.nilaiUts = nilaiUts;
+    }
+
+    public double getNilaiUts() {
+        return nilaiUts;
+    }
+
+    public void setNilaiUh(double nilaiUh) {
+        this.nilaiUh = nilaiUh;
+    }
+
+    public double getNilaiUh() {
+        return nilaiUh;
+    }
+
+    public void setNilaiUas(double nilaiUas) {
+        this.nilaiUas = nilaiUas;
+    }
+
+    public double getNilaiUas() {
+        return nilaiUas;
+    }
+
+    //apa?
+    //yang kamu edit tadi apa aja?
+    //yang megre sama quick belum diinput juga
+    //netbeans kah?
+    // di sini di intellij
+    //nggak ada
+    //coba copy aja code nya
+    //send ke wa aja gapapa
+    //sampun
+    //loh iya kah?? lah di tempatku berubah weh coba kau update mar
+    //apa aku lupa push ya?
+    //class ini belum diubah kan?
+}
